d3731fe1f91d710a753edeaaf93562f99d0903ca
Chandana Goli <cgoli@nthrive.com>
2021-02-15 15:25:10 -0600

    updating go sum file

--- Subtract/Sub.go
+++ Subtract/Sub.go
@@ -2,10 +2,12 @@ package subtract
 
 import (
 	"github.com/anugoli05/MyGoWSwithEnv/.MyPackages/mathutils"
+	
 )
+
 //CallSub is a function to call subtract function of the mygopackage to maintain my own package
-func CallSub(a, b int) int{
-var Subresult int
-Subresult=	mathutils.Subtract(a,b)
-return Subresult
+func CallSub(a, b int) int {
+	var Subresult int
+	Subresult = mathutils.Subtract(a, b)
+	return Subresult
 }
